e1056b66009a1b217e90abc24b6bbf05ed194a26
Oliver Chen <o.chen1@share.epsb.ca>
2023-07-19 16:24:14 -0600

    Many changes, optimization, aesthetics, content

--- js/SmoothScroll.js
+++ js/SmoothScroll.js
@@ -1,24 +1,29 @@
-var smoothscroll = new SmoothScroll({
-    // Scrolling Core
-    animationTime: 1000, // [ms]
-    stepSize: 100, // [px]
+if (window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth > 768) {
+    if ((('ontouchstart' in window) || (navigator.maxTouchPoints > 0) || (navigator.msMaxTouchPoints > 0))) {
+        console.log('init smooth scroll');
+        var smoothscroll = new SmoothScroll({
+            // Scrolling Core
+            animationTime: 1000, // [ms]
+            stepSize: 100, // [px]
 
-    // Acceleration
-    accelerationDelta: 20, // 50
-    accelerationMax: 2, // 3
+            // Acceleration
+            accelerationDelta: 20, // 50
+            accelerationMax: 2, // 3
 
-    // Keyboard Settings
-    keyboardSupport: true, // option
-    arrowScroll: 200, // [px]
+            // Keyboard Settings
+            keyboardSupport: true, // option
+            arrowScroll: 200, // [px]
 
-    // Pulse (less tweakable)
-    // ratio of "tail" to "acceleration"
-    pulseAlgorithm: true,
-    pulseScale: 4,
-    pulseNormalize: 1,
+            // Pulse (less tweakable)
+            // ratio of "tail" to "acceleration"
+            pulseAlgorithm: true,
+            pulseScale: 4,
+            pulseNormalize: 1,
 
-    // Other
-    touchpadSupport: true, // ignore touchpad by default
-    fixedBackground: true,
-    excluded: '',
-});
+            // Other
+            touchpadSupport: true, // ignore touchpad by default
+            fixedBackground: true,
+            excluded: '',
+        });
+    }
+}
